e7ca44f1164c07221bd4905a6971292c6a1a49d5
rwstrain <53712728+rwstrain@users.noreply.github.com>
2019-09-05 10:36:52 -0400

    Update xml_scrape.py

--- Web_Scraping/xml_scrape.py
+++ Web_Scraping/xml_scrape.py
@@ -1,17 +1,9 @@
 # @author: Wade Strain
-# XML Web Scraping project
-# Competing Through Business Analytics
+# XML Web Scraping
 # August 29, 2019
 
 import requests
 from lxml import objectify
-
-# Example:
-# Parameter: Average Temparature
-# Year: 2018
-# Month: July
-# State: Virginia
-# https://www.ncdc.noaa.gov/cag/statewide/rankings/44-tavg-201807/data.xml
 
 # parameters for Average Temp. in Virginia on August, 2016
 parameter = 'tavg'
@@ -27,11 +19,8 @@ site = fixed_site % (state, parameter, year, month)
 response = requests.get(site).content
 root = objectify.fromstring(response)
 
-my_wm_username = 'rwstrain'
-
 # data items for five-month period April-August 2016
 # multiple 'data' fields in xml file; we want 5th data field
-print(my_wm_username)
 print(root['data'][4]['value'])
 print(root['data'][4]['mean'])
 print(root['data'][4]['departure'])
